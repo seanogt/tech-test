9ba52e00d3d88fb5734a1a394495f90ad5eb9c73
James Taylor <james.taylor@easyroommate.com>
2019-08-10 00:00:23 +0100

    Refactored CustomerRepository

--- TechTest/AnyCompany.Data.Dapper/Repositories/CustomerRepository.cs
+++ TechTest/AnyCompany.Data.Dapper/Repositories/CustomerRepository.cs
@@ -1,34 +1,22 @@
-﻿using System;
+﻿using System.Configuration;
 using System.Data.SqlClient;
+using System.Linq;
 using AnyCompany.Models;
+using Dapper;
 
 namespace AnyCompany.Data.Dapper.Repositories
 {
     public static class CustomerRepository
     {
-        private static string ConnectionString = @"Data Source=(local);Database=Customers;User Id=admin;Password=password;";
-
         public static Customer Load(int customerId)
         {
-            Customer customer = new Customer();
-
-            SqlConnection connection = new SqlConnection(ConnectionString);
-            connection.Open();
-
-            SqlCommand command = new SqlCommand("SELECT * FROM Customer WHERE CustomerId = " + customerId,
-                connection);
-            var reader = command.ExecuteReader();
-
-            while (reader.Read())
+            var connectionString = ConfigurationManager.ConnectionStrings["CustomerConnectionString"].ConnectionString;
+            using (var connection = new SqlConnection(connectionString))
             {
-                customer.Name = reader["Name"].ToString();
-                customer.DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString());
-                customer.Country = reader["Country"].ToString();
+                connection.Open();
+                return connection.Query<Customer>("SELECT * FROM Customer WHERE CustomerId = @CustomerId",
+                    new {CustomerId = customerId}).FirstOrDefault();
             }
-
-            connection.Close();
-
-            return customer;
         }
     }
 }
---
+++ TechTest/AnyCompany.Ioc/Container.cs
@@ -0,0 +1,29 @@
+﻿using Castle.Windsor;
+
+namespace AnyCompany.Ioc
+{
+    public static class Container
+    {
+        private static object _objLock = new object();
+
+        private static WindsorContainer _container;
+
+        public static WindsorContainer GetContainer()
+        {
+            if (_container != null)
+                return _container;
+
+            lock (_objLock)
+            {
+                if (_container != null)
+                    return _container;
+
+                var container = new WindsorContainer();
+                container.Install(new DependencyInstaller());
+
+                _container = container;
+                return _container;
+            }
+        }
+    }
+}
