c7b32da4b1a44b18ffe6b8cd7c839348b041f75b
tomasz <tomasz.dobrowolski@cbre.com>
2019-08-11 23:45:04 +0100

    added new layers and separated ADO.NET

---
+++ TechTest/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public double Amount { get; set; }
+        public double VAT { get; set; }
+    }
+}
