6e20f147ac7712c81df011fc4d6966a2a76cb52a
Paul van Coller <paul@vancoller.com>
2020-01-28 06:34:26 +0200

    Implemented order GetByCustomerId functionality.

--- TechTest/AnyCompany/Repositories/OrderRepository.cs
+++ TechTest/AnyCompany/Repositories/OrderRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AnyCompany.Models;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace AnyCompany.Repositories
@@ -17,15 +18,45 @@ namespace AnyCompany.Repositories
             SqlConnection connection = new SqlConnection(Properties.Settings.Default.OrderConnectionString);
             connection.Open();
 
-            SqlCommand command = new SqlCommand("INSERT INTO Orders VALUES (@OrderId, @Amount, @VAT)", connection);
+            SqlCommand command = new SqlCommand("INSERT INTO Orders VALUES (@OrderId, @Amount, @VAT, @CustomerId)", connection);
 
             command.Parameters.AddWithValue("@OrderId", order.OrderId);
             command.Parameters.AddWithValue("@Amount", order.Amount);
             command.Parameters.AddWithValue("@VAT", order.VAT);
+            command.Parameters.AddWithValue("@CustomerId", order.CustomerId);
 
             command.ExecuteNonQuery();
 
             connection.Close();
+        }
+
+        public List<Order> GetAllByCustomerId(int customerId)
+        {
+            List<Order> orders = new List<Order>();
+
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.CustomerConnectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Orders WHERE CustomerId = @CustomerId", connection);
+
+            command.Parameters.AddWithValue("@CustomerId", customerId);
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                orders.Add(new Order()
+                {
+                    OrderId = int.Parse(reader["OrderId"].ToString()),
+                    Amount = double.Parse(reader["Amount"].ToString()),
+                    VAT = double.Parse(reader["VAT"].ToString()),
+                    CustomerId = int.Parse(reader["CustomerId"].ToString())
+                });
+            }
+
+            connection.Close();
+
+            return orders;
         }
     }
 }
