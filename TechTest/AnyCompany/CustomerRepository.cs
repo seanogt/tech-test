0cb02a7271275b5f8528ab57da7a7e073c279039
d9sskssssjss <42514461+silverbullet123@users.noreply.github.com>
2020-02-11 13:00:18 +0200

    Add files via upload

--- TechTest/AnyCompany/CustomerRepository.cs
+++ TechTest/AnyCompany/CustomerRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+
 
 namespace AnyCompany
 {
@@ -14,12 +16,13 @@ namespace AnyCompany
             SqlConnection connection = new SqlConnection(ConnectionString);
             connection.Open();
 
-            SqlCommand command = new SqlCommand("SELECT * FROM Customer WHERE CustomerId = " + customerId,
+            SqlCommand command = new SqlCommand("SELECT * FROM Customer WHERE  = " + customerId,
                 connection);
             var reader = command.ExecuteReader();
 
             while (reader.Read())
             {
+                customer.CustomerId = Convert.ToInt32(reader["CustomerId"].ToString());
                 customer.Name = reader["Name"].ToString();
                 customer.DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString());
                 customer.Country = reader["Country"].ToString();
@@ -28,6 +31,31 @@ namespace AnyCompany
             connection.Close();
 
             return customer;
+        }
+
+        //DRS - Added
+        //Load all customers
+        public static List<Customer> GetAllCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Customer", connection);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Customer customer = new Customer()
+                    {
+                        CustomerId = Convert.ToInt32(reader["CustomerId"].ToString()),
+                        Name = reader["Name"].ToString(),
+                        DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString()),
+                        Country = reader["Country"].ToString()
+                    };
+                    customers.Add(customer);
+                }
+            }
+            return customers;
         }
     }
 }
