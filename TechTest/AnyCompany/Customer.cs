730d67092c6f69d37e2078bd84d98bd422954ec6
Mondli02 <gobamndlela@gmail.com>
2020-03-11 20:59:33 +0200

    Update Customer.cs
    - CustomerId field added.

--- TechTest/AnyCompany/Customer.cs
+++ TechTest/AnyCompany/Customer.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 
 namespace AnyCompany
 {
     public class Customer
     {
+        public int CustomerId { get; set; }
         public string Country { get; set; }
 
         public DateTime DateOfBirth { get; set; }
