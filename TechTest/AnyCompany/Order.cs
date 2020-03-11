cac7719a2c5ebd838a909cb6161491d4631cbbc0
Mondli02 <gobamndlela@gmail.com>
2020-03-11 20:54:16 +0200

    Update Order.cs
    - CustomerId field added

--- TechTest/AnyCompany/Order.cs
+++ TechTest/AnyCompany/Order.cs
@@ -1,8 +1,10 @@
-﻿namespace AnyCompany
+ namespace AnyCompany
 {
     public class Order
     {
         public int OrderId { get; set; }
+
+        public int CustomerId { get; set; }
         public double Amount { get; set; }
         public double VAT { get; set; }
     }
