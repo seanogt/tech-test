1dd4f62f270a40eb5bccd6ae1e284b2652f380a1
nicolaas <nicolaasjvv@gmail.com>
2020-06-02 20:17:48 +0200

    Cleaned up orderService

--- TechTest/AnyCompany/Services/OrderService.cs
+++ TechTest/AnyCompany/Services/OrderService.cs
@@ -1,4 +1,5 @@
-﻿using AnyCompany.DataRepositories;
+﻿using System;
+using AnyCompany.DataRepositories;
 using AnyCompany.Models;
 
 namespace AnyCompany.Services
@@ -11,17 +12,41 @@ namespace AnyCompany.Services
         {
             CustomerModel customer = CustomerRepository.Load(customerId);
 
-            if (order.Amount == 0)
-                return false;
+            if (customer == null)
+            {
+                throw new Exception("No customer found for the supplied id");
+            }
 
-            if (customer.Country == "UK")
-                order.VAT = 0.2d;
-            else
-                order.VAT = 0;
+            if (Math.Abs(order.Amount) < 1) // using abs avoid issues with rounding
+                throw new Exception("Order ammount may not be 0");
+
+            order.VAT = GetVatForCustomer(customer);
 
             orderRepository.Save(order);
 
             return true;
         }
+
+        #region helper
+
+        /// <summary>
+        /// Gets the vat based on the customer's country.
+        /// </summary>
+        /// <param name="customer">Customer containing a country code</param>
+        /// <returns>Vat amount for customer's country</returns>
+        private double GetVatForCustomer(CustomerModel customer)
+        {
+            //note. This can be expanded later to use a proper service to retrieve the VAT as well as validate the country code
+
+            switch (customer.Country)
+            {
+                case "UK":
+                    return 0.2d;                    
+                default:
+                    return 0;                                            
+            }
+        }
+
+        #endregion
     }
 }
