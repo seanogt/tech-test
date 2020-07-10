659fc787c9dbb07679c88f2d32b04b6a7e0210e4
iamcalvinn <BkT)+7dX9VdC4gX>
2020-07-10 13:01:16 +0200

    missing files

---
+++ TechTest/AnyCompany/IServices/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnyCompany.IServices
+{
+    public interface ICustomerService
+    {
+        bool SaveCustomer(Customer customer);
+        List<Customer> GetCustomersWithOrders();
+    }
+}
