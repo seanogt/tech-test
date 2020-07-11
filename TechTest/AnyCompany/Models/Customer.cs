43c61367a213205eecd3c0de924184cd08674123
surpr <surpr@LAPTOP-6NNFSG9S>
2020-07-11 06:12:03 +0200

    Added new Address Model, also modified Customer Model

--- TechTest/AnyCompany/Models/Customer.cs
+++ TechTest/AnyCompany/Models/Customer.cs
@@ -4,10 +4,16 @@ namespace AnyCompany.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
         public string Country { get; set; }
 
         public DateTime DateOfBirth { get; set; }
 
-        public string Name { get; set; }
+        public Address Address { get; set; }
     }
 }
