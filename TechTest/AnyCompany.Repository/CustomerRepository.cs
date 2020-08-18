551fc0d2c5c6e77ab6c53742bc40b507a462d6cb
Kgaugelo Ledwaba <kgauledwaba@gmail.com>
2020-08-18 08:22:56 +0200

    Moved the customer repository class the the Repository class library and replace the hard coded sql script with a stored procedure.

---
+++ TechTest/AnyCompany.Repository/CustomerRepository.cs
@@ -0,0 +1,54 @@
+﻿using AnyCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyCompany.Repository
+{
+    public static class CustomerRepository
+    {
+        private static string ConnectionString = @"Data Source=(local);Database=Customers;User Id=admin;Password=password;";
+
+        public static Customer Load(int customerId)
+        {
+            Customer customer = new Customer();
+
+            SqlConnection connection = new SqlConnection(ConnectionString);
+
+            try
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("spGetCustomer",
+                    connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@customerId", customerId);
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    customer.Name = reader["Name"].ToString();
+                    customer.DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString());
+                    customer.Country = reader["Country"].ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return customer;
+        }
+    }
+}
