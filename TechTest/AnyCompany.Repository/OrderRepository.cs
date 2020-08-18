5449b01d4e4cdb3d484e31cde3ce9d70cbd86991
Kgaugelo Ledwaba <kgauledwaba@gmail.com>
2020-08-18 07:53:37 +0200

    Moved the Order repository to the Repository class library and Implement the IOrderRepository interface

---
+++ TechTest/AnyCompany.Repository/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using AnyCompany.Models;
+using AnyCompany.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyCompany.Repository
+{
+    internal class OrderRepository : IOrderRepository
+    {
+        private static string ConnectionString = @"Data Source=(local);Database=Orders;User Id=admin;Password=password;";
+
+        public List<Order> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Order order)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("INSERT INTO Orders VALUES (@OrderId, @Amount, @VAT)", connection);
+
+            command.Parameters.AddWithValue("@OrderId", order.OrderId);
+            command.Parameters.AddWithValue("@Amount", order.Amount);
+            command.Parameters.AddWithValue("@VAT", order.VAT);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
